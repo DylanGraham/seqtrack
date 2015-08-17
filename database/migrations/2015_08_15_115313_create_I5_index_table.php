2a0ba6e29de4a0be6f3820957bff4309a03bebba
Dylan Graham <dylan.graham@gmail.com>
2015-08-17 15:30:32 +1000

    Migrations, BatchesController

---
+++ database/migrations/2015_08_15_115313_create_I5_index_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateI5IndexTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('i5_index', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('i5_index_group_id')->unsigned();
+            $table->string('index', 50);
+            $table->string('sequence', 20);
+
+            $table->timestamps();
+
+        });
+        Schema::table('i5_index', function (Blueprint $table) {
+            $table->foreign('i5_index_group_id')->references('id')->on('i5_index_group');
+            $table->unique('index');
+            $table->unique('sequence');
+
+        });;
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('i5_index');
+    }
+}
