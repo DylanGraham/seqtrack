bbe4de4555ca1d5f834314dcc58d7f468ec556e8
Dylan Graham <dylan.graham@gmail.com>
2015-08-26 10:04:24 +1000

    Test list works

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -4,45 +4,35 @@
 @section('content')
 
 <div class="form-group">
-
 <form>
-<select name="dep">
-
+<select id="set">
     <option>Select Index Set</option>
 @foreach($IndexSet->sets() as $set)
     <option>{{ $set }}</option>
 @endforeach
-
 </select>
-    <select name="cname">
-        <option></option>
-    </select>
+<select id="seq">
+    <option></option>
+</select>
 </form>
-
 </div>
-
 
 @endsection
 
 @section('footer')
 <script>
-$(document).ready(function() {
-    $department = $("select[name='dep']");
-    $cname = $("select[name='cname']");
-     
-    $department.change(function() {
-
+$(function() {
+    var set = $('#set');  
+    var seq = $('#seq');  
+    set.change(function() {
 @for ($i=0; $i<$IndexSet->sets()->count(); $i++)
-         
-        if ($(this).selectedIndex == {{ $i }}) {
-            $("select[name='cname'] option").remove();
-            $("<option>{{ $i }}</option>").appendTo($cname);
+        if (set.prop('selectedIndex') == {{ $i }}) {
+            seq.empty().append('<option>{{ $i }}</option>');
         }
 
 @endfor
-
-    });
-});
+    })
+})
 
 </script>
 @endsection
