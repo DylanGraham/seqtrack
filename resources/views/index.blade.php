d5151940ca05638ed1c5951044ae30d2a6d1b1bd
Phillip Reynolds <s8953500@student.rmit.edu.au>
2015-09-01 22:14:11 +1000

    Add link to runs page on index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <a href="{!! route('samples.index') !!}">View Samples</a><br>
     <a href="{!! route('samples.create') !!}">Enter samples</a><br>
     <a href="{!! route('batches.index') !!}">View batches</a><br>
-
+    <a href="{!! route('runs.create') !!}">Enter runs</a><br>
     @include('errors.list')
 
 @endsection
