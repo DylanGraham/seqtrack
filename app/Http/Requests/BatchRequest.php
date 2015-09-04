ca263ff0bd2b2a1d08f6e49afcdb94e140a11806
Dylan Graham <dylan.graham@gmail.com>
2015-09-04 14:50:20 +1000

    Batch conc & volume decimals

--- app/Http/Requests/BatchRequest.php
+++ app/Http/Requests/BatchRequest.php
@@ -34,9 +34,9 @@ class BatchRequest extends Request
 
             'project_group_id' => array( 'required', 'exists:project_group,id'),
 
-            'concentration' => array('numeric', 'between:1,200', 'regex:/^[\d]{1,3}[.\d]{0,1}$/'),
+            'concentration' => array('numeric', 'between:1,200', 'regex:/^[\d]{1,3}([.][\d]){0,1}$/'),
 
-            'volume' => array('numeric', 'between:1,10000', 'regex:/^[\d]{1,5}[.\d]{0,1}$/'),
+            'volume' => array('numeric', 'between:1,10000', 'regex:/^[\d]{1,5}([.][\d]){0,1}$/'),
 
             'tube_bar_code' => array('required' , 'regex:/^[A-Z0-9]{1,60}$/' , 'max:60'),
 
