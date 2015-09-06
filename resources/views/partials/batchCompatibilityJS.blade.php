ba835065f75d28268cd67d05d626bbe815ea0172
Phillip Reynolds <s8953500@student.rmit.edu.au>
2015-09-07 00:22:04 +1000

    progressed js for adding batches to runs

--- resources/views/partials/batchCompatibilityJS.blade.php
+++ resources/views/partials/batchCompatibilityJS.blade.php
@@ -4,53 +4,114 @@
     var setbatches = $('#setbatches');
     var selectBatchesLabel = document.getElementById('selected_batches_label');
 
-    function Init() {
-        document.getElementById('selected_batches_label').innerHTML = "tttt";
-
-    }
-
-    Init();
-
-    selectedBatches.change(function () {
-
-        document.getElementById('selected_batches_label').innerHTML = 'batches selected';
-        alert("Name must be filled out");
-
-    })
 
     function checkSelectedBatches() {
-        $text ="";
-        $count=0;
-        $("input:checkbox[name=batch_check_id]:checked").each(function(){
-            $text = $text + ($(this).val())+' ';
-            $count ++;
+
+        var $count = 0;
+        var $selected = [];
+        var $notselected = [];
+
+        $("input:checkbox[name=batch_check_id]:checked").each(function () {
+
+            $selected.push($(this).val());
+            $count++;
+            this.style.display = 'block';
+
         });
-        document.getElementById('selected_batches_label').innerHTML = 'batches selected'+$text;
+        document.getElementById('selected_batches_label').innerHTML = 'batches selected ' + $selected;
 
-        $text ="";
-        $("input:checkbox[name=batch_check_id]").each(function(){
-            if (this.checked == false)
-                $text = $text + ($(this).val())+' ';
+
+        $("input:checkbox[name=batch_check_id]").each(function () {
+            if (this.checked == false) {
+
+                $notselected.push($(this).val());
+            }
+
         });
-        document.getElementById('unselected_batches_label').innerHTML = 'unbatches selected'+$text;
+        document.getElementById('unselected_batches_label').innerHTML = 'batches not selected ' + $notselected;
 
 
-        if ($count >=3)
-        {
-            $("input:checkbox[name=batch_check_id]").each(function(){
-                if (this.checked == false)
-                this.style.display = 'none';
-            });
-        }
+        var $used_sequences = [];
+        var $repeated_sequences = [];
+        var $compatible;
+        var $i5_length;
+        var $i5_used;
+        var $key;
 
-        if ($count < 3)
-        {
-            $("input:checkbox[name=batch_check_id]").each(function(){
-                if (this.checked == false)
-                    this.style.display = 'block';
-            });
-        }
-    }
+        @foreach($batches as $batch)
 
+
+            $compatible = "Compatible";
+            var $batch_sequences = [];
+            if ($selected.indexOf('{{($batch->id)}}') > -1) {
+                @foreach($batch->samples as $sample)
+
+                    @if (count($batch->samples[0]->i5_index_id) != NULL )
+
+                        $i5_length = "{{$sample->i5_index->sequence}}".length;
+                        $i5_used = true;
+                        $key = '{{($sample->i7_index->sequence)}}/{{($sample->i5_index->sequence)}}';
+
+                    @else
+
+                        $i5_length = 0;
+                        $i5_used = false;
+                        $key = "{{($sample->i7_index->sequence)}}";
+
+                    @endif
+
+                    if ($used_sequences.prop($key) == -1) {
+
+                        $batch_sequences.push($key);
+
+                    } else {
+
+                        $repeated_sequences.push($key);
+                        $compatible = "Not Compatible";
+
+                    }
+
+                @endforeach
+
+                if ($compatible == "Compatible") {
+
+                    Array.prototype.push.apply($used_sequences, $batch_sequences);
+                }
+            }
+
+
+
+        @endforeach
+
+
+        @foreach($batches as $batch)
+
+            $compatible = "Compatible";
+
+            if ($selected.indexof({{($batch->id)}})  < 0) {
+                @foreach($batch->samples as $sample)
+                    @if (count($batch->samples[0]->i5_index_id) != NULL )
+                        $i5_length = strlen({{$batch->samples[0]->i5_index_id}});
+                        $i5_used = true;
+                        $key = '{{($sample->i7_index->sequence)}}/{{($sample->i5_index->sequence)}}';
+                    @else
+                        $i5_length = 0;
+                        $i5_used = false;
+                        $key = "{{($sample->i7_index->sequence)}}";
+                    @endif
+
+                    if ($used_sequences.prop($key) == -1) {
+                        var $chks = document.querySelectorAll("input[type='checkbox'][value='{{($batch->id)}}']");
+                        document.getElementById('selected_batches_label').innerHTML = 'uncompatible batch found';
+                        $compatible = "Not Compatible";
+                        $chks.style.display = 'none';
+
+                    }
+                @endforeach
+            }
+
+        @endforeach
+
+    };
 
 </script>
