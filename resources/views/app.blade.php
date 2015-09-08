a79bd1ae7b3dbe6cc6527e12b6ed0be74b144c1c
Dylan Graham <dylan.graham@gmail.com>
2015-09-08 10:44:37 +1000

    Add success flash message

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,12 @@
                 {{ session('flash_message') }}
             </div>
         @endif
-
+        @if (Session::has('success_message'))
+            <div class="alert alert-success">
+                {{ session('success_message') }}
+            </div>
+        @endif
+        
         <div class="content">
             @yield('content')
         </div>
