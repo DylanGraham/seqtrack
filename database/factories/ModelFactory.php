c830cf77818c9249f0ac1917a8735044688b7461
Dylan Graham <dylan.graham@gmail.com>
2015-09-09 22:09:14 +1000

    Factory test

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,8 +14,24 @@
 $factory->define(App\User::class, function ($faker) {
     return [
         'name' => $faker->name,
+        'user_id' => str_random(4),
         'email' => $faker->email,
         'password' => str_random(10),
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Sample::class, function ($faker) {
+    return [
+        'password' => str_random(10),
+        'remember_token' => str_random(10),
+    ];
+});
+
+/*
+$factory->define(App\Batch::class, function ($faker) {
+    return [
+        'user_id' => factory(App\User::class)->create()->id,
+    ];
+});
+*/
