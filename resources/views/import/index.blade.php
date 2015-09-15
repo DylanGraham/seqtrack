6f7b6b354ccbab19fbf4da1a74d5dc6725b58ae6
Aakash Bhansali <s3412581@student.rmit.edu.au>
2015-09-15 12:01:08 +1000

    Raw samples import completed

---
+++ resources/views/import/index.blade.php
@@ -0,0 +1,24 @@
+@extends('app')
+@section('content')
+    <h1><a href='/'>SeqTrack</a> - Import Samples</h1>
+    <hr/>
+    @if(Session::has('success'))
+        <div class="alert-box success">
+            <h2>{!! Session::get('success') !!}</h2>
+        </div>
+    @endif
+
+    {!! Form::open(['url'=>'import/validateFile', 'files'=>true, 'method'=>'POST', 'class'=>'form-inline', ]) !!}
+    {!! Form::label('sampleFile','File',array('id'=>'','class'=>'')) !!}
+    {!! Form::file('sampleFile','',array('id'=>'','class'=>'')) !!}
+    {!! Form::submit('Submit', ['class'=>'btn btn-primary']) !!}
+    {!! Form::close() !!}
+
+    @include('errors.list')
+
+    <p class="errors">{!!$errors->first('image')!!}</p>
+    @if(Session::has('error'))
+        <p class="errors">{!! Session::get('error') !!}</p>
+    @endif
+
+@endsection
