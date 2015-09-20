01fc8f8917285aa210cb78a9a0b24b08541b0b95
Lynne Nguyen <s3433771@student.rmit.edu.au>
2015-09-20 12:41:57 +1000

    Add dropdown list for create and view for samples, batches and runs

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -12,8 +12,23 @@
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
         <li class="active"><a href="/">Home</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Create<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="{!! route('batches.create') !!}">Create Batch</a></li>
+            <li><a href="{!! route('samples.create') !!}">Create Sample</a></li>
+            <li><a href="{!! route('sampleRuns.create') !!}">Create Run</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">View<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="{!! route('batches.index') !!}">View Batch</a></li>
+            <li><a href="{!! route('samples.index') !!}">View Sample</a></li>
+            <li><a href="{!! route('runs.index') !!}">View Run</a></li>
+          </ul>
+        </li>
       </ul>
     </div><!--/.nav-collapse -->
-    
   </div>
 </nav>
