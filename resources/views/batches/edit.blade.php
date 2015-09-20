11076ceb43084916400deea56ba31928133a9ad2
Phillip Reynolds <s8953500@student.rmit.edu.au>
2015-09-20 16:50:17 +1000

    removed edit line in batches

--- resources/views/batches/edit.blade.php
+++ resources/views/batches/edit.blade.php
@@ -4,10 +4,6 @@
     <a href='/'>@include('partials.logo')</a> 
 
 
-    {!! Form::open(['url'=>'batches', 'class'=>'form-inline']) !!}
-
-    {{--@include('partials.batch', ['submitButtonText'=>'Submit'])--}}
-
     <h1> {{ $batch->batch_name }}</h1>
 
     {!! Form::model($batch, ['method'=>'PATCH', 'action'=>['BatchesController@update', $batch->id], 'class'=>'form-inline']) !!}
