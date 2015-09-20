927baf693695b38bd21e6020bb013df065e71790
Phillip Reynolds <s8953500@student.rmit.edu.au>
2015-09-20 17:50:12 +1000

    fixed git conflict in batch.edit.blade

--- resources/views/batches/edit.blade.php
+++ resources/views/batches/edit.blade.php
@@ -3,10 +3,8 @@
 @include('partials.navbar')
     <a href='/'>@include('partials.logo')</a> 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 3dc6631a07f03f7d8c5114fe4ec16a8fbb38bb64
+
     <h1> {{ $batch->batch_name }}</h1>
 
     {!! Form::model($batch, ['method'=>'PATCH', 'action'=>['BatchesController@update', $batch->id], 'class'=>'form-inline']) !!}
