e00e3dd857220394ebc04e53e7faa1029e0b4be8
Lynne Nguyen <s3433771@student.rmit.edu.au>
2015-09-21 11:27:32 +1000

    changed text colour

--- public/js/all.js
+++ public/js/all.js
@@ -16,5 +16,8 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
 
-
+$(".nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+});
 //# sourceMappingURL=all.js.map
--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -4,3 +4,7 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
 
+$(".nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+});
