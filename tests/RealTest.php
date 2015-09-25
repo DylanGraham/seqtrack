13631bb8b9479f6bc8066681d945628b9746fa74
Dylan Graham <dylan.graham@gmail.com>
2015-09-25 13:53:54 +1000

    Add create sample test

--- tests/RealTest.php
+++ tests/RealTest.php
@@ -23,15 +23,38 @@ class RealTest extends TestCase
             ->seePageIs('/samples/create');
     }
 
-/*    public function test_samples_page_form()
+    public function test_navbar()
     {
-        $this->visit('/samples/create')
-            ->type('PASTURE', 'basc_group_id')
-            ->type('AF0TJ_Cs-WW-419124R-20150109-well-D1', 'name')
-            ->type('ATTACTCG', 'i7_index_id')
-            ->type('GGCTCTGA', 'i5_index_id')
-            ->press('submit')
-            ->seePageIs('/samples');
-    }*/
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+            ->visit('/')
+            ->see('Create Batch')
+            ->see('Create Sample')
+            ->see('Create Run')
+            ->see('View Batch')
+            ->see('View Sample')
+            ->see('View Run');
+    }
+
+    public function test_samples_page_form()
+    {
+        $user = App\User::find(1);
+//        $user->super = true;
+
+        $this->actingAs($user)
+            ->visit('/samples/create')
+//            ->type('PASTURE', 'project_group_id')
+            ->select(23, 'batch_id')
+            ->type('AF0TJ_Cs-WW-419124R-20150109-well-D1', 'sample_id')
+            ->select(1, 'index_set')
+            ->select(1, 'i7_index_id')
+//          ->select('GGCTCTGA', 'i5_index_id')
+            ->type('PHPUnit', 'description')
+            ->type(5, 'runs_remaining')
+            ->type(1, 'instrument_lane')
+            ->press('Submit')
+            ->see('Index set mismatch!');
+    }
 
 }
