24119998eebb37e1d888b0e415a23d59ba3809db
Dylan Graham <dylan.graham@gmail.com>
2015-09-25 14:59:39 +1000

    Batch create tested

--- tests/RealTest.php
+++ tests/RealTest.php
@@ -37,7 +37,7 @@ class RealTest extends TestCase
             ->see('View Run');
     }
 
-    public function test_samples_page_form()
+    public function test_create_sample_mismatch()
     {
         $user = App\User::find(1);
 //        $user->super = true;
@@ -49,7 +49,6 @@ class RealTest extends TestCase
             ->type('AF0TJ_Cs-WW-419124R-20150109-well-D1', 'sample_id')
             ->select(1, 'index_set')
             ->select(1, 'i7_index_id')
-//          ->select('GGCTCTGA', 'i5_index_id')
             ->type('PHPUnit', 'description')
             ->type(5, 'runs_remaining')
             ->type(1, 'instrument_lane')
@@ -57,4 +56,23 @@ class RealTest extends TestCase
             ->see('Index set mismatch!');
     }
 
+    public function test_create_batch()
+    {
+        $user = App\User::find(1);
+
+        $this->actingAs($user)
+            ->visit('/batches/create')
+            ->type('PHPUnit-batch', 'batch_name')
+            ->type(1.5, 'concentration')
+            ->type(1.5, 'volume')
+            ->type('PHPUNITBARCODE', 'tube_bar_code')
+            ->type('PHPUnitLocation', 'tube_location')
+            ->type(55, 'tape_station_length')
+            ->type('8000-00000-00-888', 'charge_code')
+            ->select(3, 'project_group_id')
+            ->press('Submit')
+            ->dontSee('alert-danger')
+            ->seePageIs('/batches')
+            ->see('PHPUNITBARCODE');
+    }
 }
