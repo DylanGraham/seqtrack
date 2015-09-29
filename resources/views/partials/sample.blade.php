3b4691e79d5e8068ebf4000d243bd76f8d4a453a
Lynne Nguyen <s3433771@student.rmit.edu.au>
2015-09-29 16:00:21 +1000

    edit sample forms/pages

--- resources/views/partials/sample.blade.php
+++ resources/views/partials/sample.blade.php
@@ -1,86 +1,81 @@
-<form class="form-horizontal">
-    <div class="form-container">
-        <div class="row">
-            <h1>{{ $formName }}</h1>
-            @if($formName === "Create Sample")
-                <div class="form-group @if ($errors->has('sample_id')) has-error @endif">
-                    <div class="col-md-3">
-                        {!! Form::label('sample_id', 'Sample ID') !!}
-                    </div>
-                    <div class="col-md-9">
-                        {!! Form::text('sample_id', null, ['data-toggle'=>'tooltip', 'title'=>'Sample name','class'=>'form-control', 'placeholder'=>'Sample name']) !!}
-                    </div>
-                </div>
-            @endif   
-                <div class="from-group @if ($errors->has('plate')) has-error @endif">
-                    <div class="col-md-3">
-                        {!! Form::label('plate', 'Plate') !!}
-                    </div>
-                    <div class="col-md-9">
-                        {!! Form::text('plate', null, ['data-toggle'=>'tooltip', 'title'=>'Plate','class'=>'form-control', 'placeholder'=>'Plate']) !!}
-                    </div>
-                </div>
-                <div class="form-group @if ($errors->has('well')) has-error @endif">
-                    <div class="col-md-3">
-                        {!! Form::label('well', 'Well') !!}
-                    </div>
-                    <div class="col-md-9">
-                        {!! Form::text('well', null, ['data-toggle'=>'tooltip', 'title'=>'Well','class'=>'form-control', 'placeholder'=>'Well']) !!}
-                    </div>
-                </div>
-            @if ($formName === "Create Sample")
-                <div class="form-group @if ($errors->has('index_set')) has-error @endif">
-                    <div class="col-md-3">
-                        {!! Form::label('index_set', 'Index Set', ['data-toggle'=>'tooltip', 'title'=>'Index set']) !!}
-                    </div>
-                    <div class="col-md-9">
-                        {!! Form::select('index_set', $iSet, null, ['data-toggle'=>'tooltip', 'title'=>'Index set','class'=>'form-control']) !!}
-                    </div>
-                </div>
-                <div class="form-group @if ($errors->has('i7_index_id')) has-error @endif">
-                    <div class="col-md-3">
-                        {!! Form::label('i7_index_id', 'i7 Index ID') !!}
-                    </div>
-                    <div class="col-md-9">
-                        {!! Form::select('i7_index_id', $i7, ['name'=>'i7_index_id'], ['data-toggle'=>'tooltip', 'title'=>'I7 Index','class'=>'form-control']) !!}
-                    </div>
-                </div>
-                <!--<div class="form-group @if ($errors->has('i5_index_id')) has-error @endif">
-                    <div class="col-md-3">
-                        {!! Form::label('i5_index_id', 'i5 Index ID') !!}
-                    </div>
-                    <div class="col-md-9">
-                        {!! Form::select('i5_index_id', $i5, ['name'=>'i5_index_id'], ['data-toggle'=>'tooltip', 'title'=>'I5 index','class'=>'form-control']) !!}
-                    </div>
-                </div>-->
-            @endif
-                <div class="form-group @if ($errors->has('description')) has-error @endif">
-                    <div class="col-md-3">
-                        {!! Form::label('description', 'Description') !!}
-                    </div>
-                    <div class="col-md-9">
-                        {!! Form::text('description', null, ['title'=>'Description','class'=>'form-control', 'placeholder'=>'Description']) !!}
-                    </div>
-                </div>
-                <div class="form-group @if ($errors->has('runs_remaining')) has-error @endif">
-                    <div class="col-md-3">
-                        {!! Form::label('runs_remaining', 'Runs Remaining') !!}
-                    </div>
-                    <div class="col-md-9">
-                        {!! Form::number('runs_remaining', null, ['data-toggle'=>'tooltip', 'title'=>'Runs','class'=>'form-control', 'placeholder'=>'Runs']) !!}
-                    </div>
-                </div>
-                <div class="form-group @if ($errors->has('instrument_lane')) has-error @endif">
-                    <div class="col-md-3">
-                        {!! Form::label('instrument_lane', 'Instrument Lane') !!}
-                    </div>
-                    <div class="col-md-9">
-                        {!! Form::number('instrument_lane', null, ['data-toggle'=>'tooltip', 'title'=>'Instrument lane','class'=>'form-control', 'placeholder'=>'Instrument lane']) !!}
-                    </div>
-                </div>
-            <div class="col-md-offset-3 col-md-9">
-                {!! Form::submit($submitButtonText, ['class'=>'btn btn-primary']) !!}
+<div class="row">
+    @if($formName === "Create Sample")
+        <div class="form-group @if ($errors->has('sample_id')) has-error @endif">
+            <div class="col-md-3">
+                {!! Form::label('sample_id', 'Sample ID') !!}
+            </div>
+            <div class="col-md-9">
+                {!! Form::text('sample_id', null, ['data-toggle'=>'tooltip', 'title'=>'Sample name','class'=>'form-control', 'placeholder'=>'Sample name']) !!}
             </div>
         </div>
+    @endif   
+        <div class="from-group @if ($errors->has('plate')) has-error @endif">
+            <div class="col-md-3">
+                {!! Form::label('plate', 'Plate') !!}
+            </div>
+            <div class="col-md-9">
+                {!! Form::text('plate', null, ['data-toggle'=>'tooltip', 'title'=>'Plate','class'=>'form-control', 'placeholder'=>'Plate']) !!}
+            </div>
+        </div>
+        <div class="form-group @if ($errors->has('well')) has-error @endif">
+            <div class="col-md-3">
+                {!! Form::label('well', 'Well') !!}
+            </div>
+            <div class="col-md-9">
+                {!! Form::text('well', null, ['data-toggle'=>'tooltip', 'title'=>'Well','class'=>'form-control', 'placeholder'=>'Well']) !!}
+            </div>
+        </div>
+    @if ($formName === "Create Sample")
+        <div class="form-group @if ($errors->has('index_set')) has-error @endif">
+            <div class="col-md-3">
+                {!! Form::label('index_set', 'Index Set', ['data-toggle'=>'tooltip', 'title'=>'Index set']) !!}
+            </div>
+            <div class="col-md-9">
+                {!! Form::select('index_set', $iSet, null, ['data-toggle'=>'tooltip', 'title'=>'Index set','class'=>'form-control']) !!}
+            </div>
+        </div>
+        <div class="form-group @if ($errors->has('i7_index_id')) has-error @endif">
+            <div class="col-md-3">
+                {!! Form::label('i7_index_id', 'i7 Index ID') !!}
+            </div>
+            <div class="col-md-9">
+                {!! Form::select('i7_index_id', $i7, ['name'=>'i7_index_id'], ['data-toggle'=>'tooltip', 'title'=>'I7 Index','class'=>'form-control']) !!}
+            </div>
+        </div>
+        <!--<div class="form-group @if ($errors->has('i5_index_id')) has-error @endif">
+            <div class="col-md-3">
+                {!! Form::label('i5_index_id', 'i5 Index ID') !!}
+            </div>
+            <div class="col-md-9">
+                {!! Form::select('i5_index_id', $i5, ['name'=>'i5_index_id'], ['data-toggle'=>'tooltip', 'title'=>'I5 index','class'=>'form-control']) !!}
+            </div>
+        </div>-->
+    @endif
+        <div class="form-group @if ($errors->has('description')) has-error @endif">
+            <div class="col-md-3">
+                {!! Form::label('description', 'Description') !!}
+            </div>
+            <div class="col-md-9">
+                {!! Form::text('description', null, ['title'=>'Description','class'=>'form-control', 'placeholder'=>'Description']) !!}
+            </div>
+        </div>
+        <div class="form-group @if ($errors->has('runs_remaining')) has-error @endif">
+            <div class="col-md-3">
+                {!! Form::label('runs_remaining', 'Runs Remaining') !!}
+            </div>
+            <div class="col-md-9">
+                {!! Form::number('runs_remaining', null, ['data-toggle'=>'tooltip', 'title'=>'Runs','class'=>'form-control', 'placeholder'=>'Runs']) !!}
+            </div>
+        </div>
+        <div class="form-group @if ($errors->has('instrument_lane')) has-error @endif">
+            <div class="col-md-3">
+                {!! Form::label('instrument_lane', 'Instrument Lane') !!}
+            </div>
+            <div class="col-md-9">
+                {!! Form::number('instrument_lane', null, ['data-toggle'=>'tooltip', 'title'=>'Instrument lane','class'=>'form-control', 'placeholder'=>'Instrument lane']) !!}
+            </div>
+        </div>
+    <div class="col-md-offset-3 col-md-9">
+        {!! Form::submit($submitButtonText, ['class'=>'btn btn-primary']) !!}
     </div>
-</form>
+</div>
--- resources/views/samples/create.blade.php
+++ resources/views/samples/create.blade.php
@@ -1,21 +1,21 @@
 @extends('app')
 @section('content')
 @include('partials.navbar')
-
+<div class="heading-container">
+    <h1>Create Sample</h1>
+</div>
 @if (count($user->batches))
-    <div class="box-container">
-        <h4>Select a batch to add samples to:<h4>
+    <div class="form-container">
+        <h3>Select a batch to add samples to:</h3>
         <div class="col-md-8">{!! Form::open(['url'=>'samples', 'class'=>'form-inline']) !!}</div>
         @include('partials.batchSelect')
-        <p>or</p> 
-        <a href="{!! route('batches.create') !!}">Create A New Batch</a>
-    </div>
-    
-
+        <p>or</p>
+        <h3><a href="{!! route('batches.create') !!}">Create A New Batch</a></h3>
+        <br/>
     @include('partials.sample', ['formName'=>'Create Sample', 'submitButtonText'=>'Submit'])
 
     {!! Form::close() !!}
-
+    </div>
 @else
 
     <br><br>
