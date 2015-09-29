133e19d3f882154e80a43d19749fd4dd852b1ea7
Aakash Bhansali <s3412581@student.rmit.edu.au>
2015-09-29 18:09:45 +1000

    Added a request controller for Import Sample Request

---
+++ app/Http/Requests/ImportSampleRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class ImportSampleRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+
+            'sampleFile' => array('required', 'mimes:csv,txt'),
+
+            'plate' => array('regex:/^[a-zA-Z0-9-]{1,120}$/' , 'max:120'),
+
+            'well' => array('regex:/^[a-zA-Z0-9-]{1,120}$/', 'max:120'),
+
+            'description' => array('regex:/^[a-zA-Z0-9-]{1,120}$/' , 'max:120'),
+
+            'runs_remaining' => array('required', 'integer', 'between:1,60'),
+
+            'instrument_lane' => array('required', 'integer', 'between:1,8'),
+
+        ];
+    }
+}
