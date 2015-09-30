c5d9d7d0091ba74863510107e01e032853dcadd9
Dylan Graham <dylan.graham@gmail.com>
2015-10-01 09:46:07 +1000

    Create and show run

--- tests/RealTest.php
+++ tests/RealTest.php
@@ -349,4 +349,41 @@ class RealTest extends TestCase
             ->see('Import Samples');
     }
 
+    public function test_runs_page()
+    {
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+            ->visit('/runs')
+            ->seePageIs('/runs')
+            ->see('Runs');
+    }
+
+    public function test_create_and_show_run()
+    {
+        $user = App\User::find(1);
+
+        $input = [
+            'batch_check_id' => [2, 3]
+        ];
+
+        $this->actingAs($user)
+            ->visit('/sampleRuns/create')
+            ->submitForm('Next -> Enter run details', $input)
+            ->seePageIs('/runDetails/create')
+            ->type('TestExperiment', 'experiment_name')
+            ->type('Desc', 'description')
+            ->type('111', 'read1')
+            ->type('222', 'read2')
+            ->type('FCID', 'flow_cell')
+            ->press('Submit')
+//            ->seePageIs('/runs') /* Currently not redirecting to runs */
+            ->visit('/runs')
+            ->see('TestExperiment')
+            ->seeInDatabase('runs', ['experiment_name' => 'TestExperiment'])
+            ->visit('/runs/1')
+            ->see('TestExperiment');
+    }
+
+
 }
