6217665113a6d4666c679c2a62805478acc28738
Dylan Graham <dylan.graham@gmail.com>
2015-10-03 16:59:33 +1000

    Remove instrument_lane

--- tests/RealTest.php
+++ tests/RealTest.php
@@ -55,7 +55,6 @@ class RealTest extends TestCase
             ->select(1, 'i7_index_id')
             ->type('PHPUnit', 'description')
             ->type(5, 'runs_remaining')
-            ->type(1, 'instrument_lane')
             ->press('Submit')
             ->see('Index set mismatch!');
     }
