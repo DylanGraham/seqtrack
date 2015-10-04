c1fe51801a6e35ec2b28227c19beb3bb27830709
Phillip Reynolds <s8953500@student.rmit.edu.au>
2015-10-05 10:51:36 +1100

    updated login page css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,77 @@
 @extends('app')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div>
-				<div>
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-					<img class="center-block" src="/images/logo.png" alt="SeqTrack Logo" style="width:60%;height:60%;padding:30px;">
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-offset-1">
 
-<!--						<div class="form-group">
+                <div>
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <img class="center-block" src="/images/logo.png" alt="SeqTrack Logo"
+                         style="width:60%;height:60%;padding:30px;">
+                </div>
+                <div class="col-md-offset-2">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                        <!--						<div class="form-group">
 							<label class="col-md-4 control-label">E-Mail Address</label>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
 							</div>
 						</div> -->
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">User ID</label>
-							<div class="col-md-6">
-								<input type="user_id" class="form-control" name="user_id" value="{{ old('user_id') }}">
-							</div>
-						</div>
+                        <div class="form-group ">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
+                            <div class="col-md-3">
+                                {!! Form::label('user_id', 'User ID') !!}
+                            </div>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
-								</div>
-								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
-							</div>
-						</div>
+                            <div class="col-md-6">
+                                <input type="user_id" class="form-control" name="user_id" value="{{ old('user_id') }}">
+                            </div>
+                            <br/>
+                            <br/>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Login</button>
+                            <div class="col-md-3">
+                                {!! Form::label('password', 'Password') !!}
+                            </div>
 
-								
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password">
+                            </div>
+
+                            <div class="col-md-6 col-md-offset-3">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember"> Remember Me
+                                    </label>
+                                    <a class="" href="{{ url('/password/email') }}">Forgot Your Password?</a>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">Login</button>
+
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 @endsection
