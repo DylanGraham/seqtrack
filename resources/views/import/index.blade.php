1cca113403bbab9a6ded249435434e4fd17cf382
Dylan Graham <dylan.graham@gmail.com>
2015-10-05 15:38:22 +1100

    Import styling

--- resources/views/import/index.blade.php
+++ resources/views/import/index.blade.php
@@ -8,43 +8,61 @@
     @endif
 
     @if (count($user->batches))
+    <div class="form-container">
+    <h1>Import Samples</h1>
 
-        <h3>Import Samples</h3>
-
-        <p>Select a batch to add samples to:</p>
+    <h4>Select a batch to add samples to, or
+    <a href="{!! route('batches.create') !!}">create a new batch</a></h4>
 
         {!! Form::open(['url'=>'import/validateFile', 'files'=>true, 'method'=>'POST', 'class'=>'form-inline', ]) !!}
 
-        @include('partials.batchSelect')
-        <span>or</span> <a href="{!! route('batches.create') !!}">create a new batch</a>
+        @include('partials.batchSelectImport')
 
-        <hr/>
-        <div class="form-group">
-            <span class="group @if ($errors->has('plate')) has-error @endif">
-            {!! Form::label('plate', 'plate', ['class'=>'sr-only']) !!}
-                {!! Form::text('plate', null, ['data-toggle'=>'tooltip', 'title'=>'Plate','class'=>'form-control', 'placeholder'=>'Plate']) !!}
-            </span>
-            <span class="group @if ($errors->has('well')) has-error @endif">
-            {!! Form::label('well', 'well', ['class'=>'sr-only']) !!}
-                {!! Form::text('well', null, ['data-toggle'=>'tooltip', 'title'=>'Well','class'=>'form-control', 'placeholder'=>'Well']) !!}
-            </span>
-            <span class="group @if ($errors->has('description')) has-error @endif">
-            {!! Form::label('description', 'description', ['class'=>'sr-only']) !!}
-                {!! Form::text('description', null, ['data-toggle'=>'tooltip', 'title'=>'Description','class'=>'form-control', 'placeholder'=>'Description']) !!}
-            </span>
-            <span class="group @if ($errors->has('runs_remaining')) has-error @endif">
-            {!! Form::label('runs_remaining', 'Runs_remaining', ['class'=>'sr-only']) !!}
-                {!! Form::number('runs_remaining', null, ['data-toggle'=>'tooltip', 'title'=>'Runs','class'=>'form-control', 'placeholder'=>'Runs']) !!}
-            </span>
-            <br><br>
-            <span class="group @if ($errors->has('sampleFile')) has-error @endif">
-            {!! Form::label('file', 'file', ['class'=>'sr-only']) !!}
-                {!! Form::file('sampleFile', null, ['data-toggle'=>'tooltip', 'title'=>'Upload the file','class'=>'form-control', 'placeholder'=>'Upload the file']) !!}
-            </span>
+            <div class="group @if ($errors->has('plate')) has-error @endif">
+            <div class="col-md-3">
+            {!! Form::label('plate', 'Plate') !!}
+            </div>
+            <div class="col-md-9">
+            {!! Form::text('plate', null, ['class'=>'form-control', 'placeholder'=>'Plate']) !!}
+            </div>
+            </div>
+            <div class="group @if ($errors->has('well')) has-error @endif">
+            <div class="col-md-3">
+            {!! Form::label('well', 'Well') !!}
+            </div>
+            <div class="col-md-9">
+            {!! Form::text('well', null, ['class'=>'form-control', 'placeholder'=>'Well']) !!}
+            </div>
+            </div>
+            <div class="group @if ($errors->has('description')) has-error @endif">
+            <div class="col-md-3">
+            {!! Form::label('description', 'Description') !!}
+            </div>
+            <div class="col-md-9">
+            {!! Form::text('description', null, ['class'=>'form-control', 'placeholder'=>'Description']) !!}
+            </div>
+            </div>
+            <div class="group @if ($errors->has('runs_remaining')) has-error @endif">
+            <div class="col-md-3">
+            {!! Form::label('runs_remaining', 'Runs remaining') !!}
+            </div>
+            <div class="col-md-9">
+            {!! Form::number('runs_remaining', null, ['class'=>'form-control', 'placeholder'=>'Runs']) !!}
+            </div>
+            </div>
+            <div class="group @if ($errors->has('sampleFile')) has-error @endif">
+            <div class="col-md-3">
+            {!! Form::label('file', 'File') !!}
+            </div>
+            <div class="col-md-9">
+                {!! Form::file('sampleFile', null, ['class'=>'form-control', 'placeholder'=>'Upload the file']) !!}
+            </div>
+            </div>
+            <div class="col-md-offset-3 col-md-9">
             {!! Form::submit('Submit', ['class'=>'btn btn-primary']) !!}
-        </div>
+            </div>
+       </div> 
             {!! Form::close() !!}
-        
     @else
 
         <br><br>
---
+++ resources/views/partials/batchSelectImport.blade.php
@@ -0,0 +1,6 @@
+    <div class="col-md-3">
+        {!! Form::label('batch_id', 'Batch name') !!}
+    </div>
+    <div class="col-md-9">
+        {!! Form::select('batch_id', $batches, null, ['class'=>'form-control']) !!}
+    </div>
