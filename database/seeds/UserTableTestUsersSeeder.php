69f15784897d5250613ff77147c56abd0886f4bc
Dylan Graham <dylan.graham@gmail.com>
2015-11-24 10:19:12 +1100

    Add test users

--- database/seeds/UserTableTestUsersSeeder.php
+++ database/seeds/UserTableTestUsersSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
 
-class UserTableSeeder extends Seeder
+class UserTableTestUsersSeeder extends Seeder
 {
     /**
      * Run the database seeds.
