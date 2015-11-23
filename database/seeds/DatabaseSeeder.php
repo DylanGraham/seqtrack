097f4a0b94c90a6351727cea8c3a41444217c35d
Dylan Graham <dylan.graham@gmail.com>
2015-11-24 10:17:14 +1100

    Add test users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-      $this->call(UserTableSeeder::class);
+      $this->call(UserTableTestUsersSeeder::class);
       $this->call(IndexSetTableSeeder::class);
       $this->call(I7TableSeeder::class);
       $this->call(I5TableSeeder::class);
---
+++ database/seeds/UserTableTestUsersSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Dylan',
+            'email' => 'dylan@example.com',
+            'user_id' => 'abcd',
+            'password' => bcrypt('password'),
+            'super' => '1',
+            'default_project_id' => '1',
+            'default_charge_code' => '8000-00000-00-001',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Phillip',
+            'email' => 'phillip@example.com',
+            'user_id' => 'efgh',
+            'password' => bcrypt('password'),
+            'super' => '0',
+            'default_project_id' => '4',
+            'default_charge_code' => "8338-11389-21-379",
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
