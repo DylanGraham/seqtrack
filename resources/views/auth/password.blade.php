511010f7fdb483d0a913e0c4f406b01626543e57
Dylan Graham <dylan.graham@gmail.com>
2015-11-25 17:39:32 +1100

    Make password reset page look nice!

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,12 +1,8 @@
 @extends('app')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Reset Password</div>
-				<div class="panel-body">
+			<div class="form-container">
+				<h1>Reset Password</h1>
 					@if (session('status'))
 						<div class="alert alert-success">
 							{{ session('status') }}
@@ -24,11 +20,13 @@
 						</div>
 					@endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+					<form role="form" method="POST" action="{{ url('/password/email') }}">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
+							<div class="col-md-6">
+							    <label>E-Mail Address</label>
+							</div>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
 							</div>
@@ -42,9 +40,5 @@
 							</div>
 						</div>
 					</form>
-				</div>
 			</div>
-		</div>
-	</div>
-</div>
 @endsection
