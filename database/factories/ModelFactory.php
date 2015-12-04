c6da4f2e84c8c6339167b4ce01abd9db95917a6d
Dylan Graham <dylan.graham@gmail.com>
2015-12-04 14:58:03 +1100

    Sample and batch factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Carbon\Carbon;
+
 $factory->define(App\User::class, function ($faker) {
     return [
         'name' => $faker->name,
@@ -21,16 +23,35 @@ $factory->define(App\User::class, function ($faker) {
     ];
 });
 
-$factory->define(App\Sample::class, function ($faker) {
+$factory->define(App\Batch::class, function ($faker) {
     return [
-        'sample_id' => str_random(10),
+        'user_id' => '1',  // factory(App\User::class)->create()->id,
+        "batch_name"    =>  str_random(16),
+        "project_group_id" => "1",
+        "concentration" =>  "0.0",
+        "volume"        =>  "0.0",
+        "tube_bar_code" =>  "TBC",
+        "tube_location" =>  "TL",
+        "tape_station_length" =>  "0.0",
+        "charge_code"   =>  "1111-11111-11-111",
+        "created_at"    =>  Carbon::now(),
+        "updated_at"    =>  Carbon::now(),
     ];
 });
 
-/*
-$factory->define(App\Batch::class, function ($faker) {
+$factory->define(App\Sample::class, function ($faker) {
     return [
-        'user_id' => factory(App\User::class)->create()->id,
+        'batch_id'=> $faker->numberBetween(2, 10),
+        'sample_id' => str_random(12),
+        'sample_id_suffix'=>'1',
+        'plate'=>'plate1',
+        'well'=>'well1',
+        'i7_index_id'=>'193',
+        'i5_index_id'=>'1',
+        'description'=>'description1',
+        'runs_remaining'=>'16',
+        'instrument_lane'=>'8',
+        'created_at'=>  Carbon::now(),
+        'updated_at'=>Carbon::now(),
     ];
 });
-*/
