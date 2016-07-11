6a5af788f8431d8b3c7e2c0136e35d7544f1c0bd
Dylan Graham <dylan.graham@gmail.com>
2016-07-11 15:20:02 +1000

    formgroup to help Plate field display properly

--- resources/views/partials/batchSelectImport.blade.php
+++ resources/views/partials/batchSelectImport.blade.php
@@ -1,6 +1,8 @@
+<div class="form-group">
     <div class="col-md-3">
         {!! Form::label('batch_id', 'Batch name') !!}
     </div>
     <div class="col-md-9">
         {!! Form::select('batch_id', $batches, null, ['class'=>'form-control']) !!}
     </div>
+</div>
